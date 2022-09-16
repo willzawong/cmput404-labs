f224182f39f0e015c1487c3283cc44ea61e50691
William Wong <56713153+willzawong@users.noreply.github.com>
2022-09-15 21:50:18 -0600

    download own code and print

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,5 +1,5 @@
 import requests
 print(requests.__version__)
 
-request = requests.get('https://www.google.ca/')
-print(request)
+request = requests.get('https://raw.githubusercontent.com/willzawong/cmput404-labs/main/lab1/lab1.py')
+print(request.content)
